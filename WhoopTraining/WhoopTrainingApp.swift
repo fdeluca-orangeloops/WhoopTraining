02f0c1fbaf218361c6d44ee7319e6dfd9eda756a
Federico De Luca <fdeluca@orangeloops.com>
2023-06-21 16:34:54 -0300

    Initial Commit

---
+++ WhoopTraining/WhoopTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoopTrainingApp.swift
+//  WhoopTraining
+//
+//  Created by Federico De Luca on 21/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WhoopTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
